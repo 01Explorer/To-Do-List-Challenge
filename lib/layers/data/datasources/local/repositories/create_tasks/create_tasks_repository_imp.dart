e188b63e57c2f6982854eac65fe38b83fc7ffc20
Gabriel Santos <gabrielsantostca@gmail.com>
2022-11-12 13:59:24 -0300

    feat(create_tasks): developed the repository implementation

---
+++ lib/layers/data/datasources/local/repositories/create_tasks/create_tasks_repository_imp.dart
@@ -0,0 +1,20 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_list_challenge/layers/data/datasources/local/dao/task_entity_dao.dart';
+import 'package:to_do_list_challenge/layers/domain/entities/task_entity.dart';
+import 'package:to_do_list_challenge/layers/domain/repositories/create_tasks/create_tasks_repository.dart';
+
+class CreateTasksRepositoryImp implements CreateTasksRepository {
+  final Box<TaskEntity> hiveBox;
+
+  CreateTasksRepositoryImp(this.hiveBox);
+  @override
+  bool call(String title, String description, DateTime expiryDate) {
+    TaskEntityDao task = TaskEntityDao(
+        taskTitle: title, content: description, endDate: expiryDate);
+    if (hiveBox.containsKey(title.toLowerCase())) {
+      return false;
+    }
+    hiveBox.put(title.toLowerCase(), task);
+    return true;
+  }
+}
