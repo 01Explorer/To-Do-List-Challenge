9ac85fea6463739c2385f760b6b0bd205cdf67aa
Gabriel Santos <gabrielsantostca@gmail.com>
2022-11-12 14:00:11 -0300

    feat(filter_tasks_by_date_between): developed the repository implementation

---
+++ lib/layers/data/datasources/local/repositories/filter_tasks_by_date_between/filter_tasks_by_date_between_repository_imp.dart
@@ -0,0 +1,17 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_list_challenge/layers/domain/entities/task_entity.dart';
+import 'package:to_do_list_challenge/layers/domain/repositories/filter_tasks_by_date_between/filter_tasks_by_date_between_repository.dart';
+
+class FilterTasksByDateBetweenRepositoryImp
+    implements FilterTasksByDateBetweenRepository {
+  final Box<TaskEntity> hiveBox;
+
+  FilterTasksByDateBetweenRepositoryImp(this.hiveBox);
+  @override
+  List<TaskEntity> call(DateTime startDate, DateTime endDate) {
+    return hiveBox.values
+        .where((e) =>
+            e.expiryDate.isAfter(startDate) && e.expiryDate.isBefore(endDate))
+        .toList();
+  }
+}
