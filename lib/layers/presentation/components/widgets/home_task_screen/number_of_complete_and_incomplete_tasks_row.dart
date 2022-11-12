cdbf91f356d8c50cafbba94bc841530128b2322f
Gabriel Santos <gabrielsantostca@gmail.com>
2022-11-12 14:10:13 -0300

    refactor(home_task_screen): refactor one of the page sections

---
+++ lib/layers/presentation/components/widgets/home_task_screen/number_of_complete_and_incomplete_tasks_row.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list_challenge/layers/domain/entities/task_entity.dart';
+import 'package:to_do_list_challenge/layers/presentation/controllers/home_controller.dart';
+import 'package:to_do_list_challenge/layers/presentation/controllers/task_controller.dart';
+
+class NumberOfCompleteAndIncompleteTasksRow extends StatelessWidget {
+  final TaskController _taskController;
+  const NumberOfCompleteAndIncompleteTasksRow(this._taskController, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<TaskEntity> incompleteTasksList = _taskController.getIncompleteTasks();
+    List<TaskEntity> completeTasksList = _taskController.getCompletedTasks();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+            'Tasks: ${incompleteTasksList.length} incompletes and ${completeTasksList.length} done'),
+      ],
+    );
+  }
+}
