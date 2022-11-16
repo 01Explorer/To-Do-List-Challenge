6d26a6603d6c389a916294e2fd468c5535808d03
Gabriel Santos <gabrielsantostca@gmail.com>
2022-11-15 23:33:53 -0300

    feat(app_theme): created and set new colors for both themes

--- lib/layers/presentation/themes/app_theme.dart
+++ lib/layers/presentation/themes/app_theme.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_list_challenge/layers/presentation/themes/theme_colors.dart';
 
-ThemeData themeToDo = ThemeData(
+ThemeData themeToDoDark = ThemeData(
   brightness: Brightness.dark,
   fontFamily: GoogleFonts.inter().fontFamily,
-  scaffoldBackgroundColor: ThemeColors.backgorundColor,
+  scaffoldBackgroundColor: ThemeColors.backgorundColorDark,
   primarySwatch: ThemeColors.primaryAppColor,
   textTheme: const TextTheme(
     headlineLarge: TextStyle(
       fontWeight: FontWeight.w700,
       fontSize: 32,
-      color: ThemeColors.headerFontColor,
+      color: ThemeColors.headerFontColorDark,
     ),
     bodyMedium: TextStyle(
         fontWeight: FontWeight.w600,
@@ -20,18 +20,56 @@ ThemeData themeToDo = ThemeData(
     headlineSmall: TextStyle(
       fontWeight: FontWeight.w700,
       fontSize: 18,
-      color: ThemeColors.sectionTitleFontColor,
+      color: ThemeColors.sectionTitleFontColorDark,
     ),
     bodySmall: TextStyle(
         fontWeight: FontWeight.w600,
         fontSize: 16,
-        color: ThemeColors.headerFontColor),
+        color: ThemeColors.headerFontColorDark),
+  ),
+  checkboxTheme: CheckboxThemeData(
+    fillColor: MaterialStateProperty.all<Color>(
+        ThemeColors.checkBoxUnselectedColorDark),
+    checkColor:
+        MaterialStateProperty.all<Color>(ThemeColors.sectionTitleFontColorDark),
+    visualDensity: VisualDensity.compact,
+    splashRadius: 20,
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: ThemeColors.primaryAppColor,
+    iconSize: 32,
+  ),
+);
+
+ThemeData themeToDoLight = ThemeData(
+  brightness: Brightness.light,
+  fontFamily: GoogleFonts.inter().fontFamily,
+  primarySwatch: ThemeColors.primaryAppColor,
+  textTheme: const TextTheme(
+    headlineLarge: TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 32,
+      color: ThemeColors.headerFontColorLight,
+    ),
+    bodyMedium: TextStyle(
+        fontWeight: FontWeight.w600,
+        fontSize: 14,
+        color: ThemeColors.subtitleTaskTextColorLight),
+    headlineSmall: TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 18,
+      color: ThemeColors.sectionTitleFontColorLight,
+    ),
+    bodySmall: TextStyle(
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+        color: ThemeColors.headerSubstitleFontColor),
   ),
   checkboxTheme: CheckboxThemeData(
     fillColor:
-        MaterialStateProperty.all<Color>(ThemeColors.checkBoxUnselectedColor),
+        MaterialStateProperty.all<Color>(ThemeColors.headerFontColorDark),
     checkColor:
-        MaterialStateProperty.all<Color>(ThemeColors.sectionTitleFontColor),
+        MaterialStateProperty.all<Color>(ThemeColors.backgorundColorDark),
     visualDensity: VisualDensity.compact,
     splashRadius: 20,
   ),
--- lib/layers/presentation/themes/theme_colors.dart
+++ lib/layers/presentation/themes/theme_colors.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-  static const backgorundColor = Color.fromRGBO(20, 20, 25, 1);
+  static const backgorundColorDark = Color.fromRGBO(20, 20, 25, 1);
   static const primaryAppColor = Colors.blue;
-  static const headerFontColor = Color.fromRGBO(218, 218, 218, 1);
+  static const headerFontColorDark = Color.fromRGBO(218, 218, 218, 1);
+  static const headerFontColorLight = Color.fromRGBO(41, 43, 53, 1);
   static const headerSubstitleFontColor = Color.fromRGBO(87, 87, 103, 1);
-  static const sectionTitleFontColor = Color.fromRGBO(235, 235, 235, 1);
-  static const checkBoxUnselectedColor = Color.fromRGBO(41, 43, 53, 1);
+  static const sectionTitleFontColorDark = Color.fromRGBO(235, 235, 235, 1);
+  static const sectionTitleFontColorLight = Color.fromRGBO(87, 87, 103, 1);
+  static const subtitleTaskTextColorLight = Color.fromRGBO(185, 185, 190, 1);
+  static const checkBoxUnselectedColorDark = Color.fromRGBO(41, 43, 53, 1);
 }
