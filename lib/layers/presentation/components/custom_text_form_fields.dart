7e2f1ab7ece429b9fb5a1dcc5adaac35de105925
Gabriel Santos <gabrielsantostca@gmail.com>
2023-01-25 16:39:48 -0300

    feat(custom_text_form_fields): added more functionalities to the component

--- lib/layers/presentation/components/custom_text_form_fields.dart
+++ lib/layers/presentation/components/custom_text_form_fields.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormFields extends StatelessWidget {
-  final TextEditingController _textEditingController;
+  final TextEditingController? textEditingController;
+  final String? initialValue;
   final String labelText;
+  final Function(String)? onChanged;
 
-  const CustomTextFormFields(
-    this._textEditingController, {
+  const CustomTextFormFields({
+    this.textEditingController,
     Key? key,
     required this.labelText,
+    this.initialValue,
+    this.onChanged,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      controller: _textEditingController,
+      controller: textEditingController,
+      initialValue: initialValue,
       decoration: InputDecoration(
         labelText: labelText,
       ),
       validator: (value) {
         return value == null ? 'This entry can\'t be null' : null;
       },
+      onChanged: onChanged,
     );
   }
 }
