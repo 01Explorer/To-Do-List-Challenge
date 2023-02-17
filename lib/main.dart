80f714017a5f3b138e8037a19f8918d28e2b553c
Gabriel Santos <gabrielsantostca@gmail.com>
2023-02-16 22:20:21 -0300

    refactor(main): changed the initial screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:to_do_list_challenge/layers/data/datasources/local/dao/task_entity_dao.dart';
 import 'package:to_do_list_challenge/layers/domain/entities/task_entity.dart';
+import 'package:to_do_list_challenge/layers/presentation/controllers/land_page_controller.dart';
 import 'package:to_do_list_challenge/layers/presentation/controllers/theme_manager_controller.dart';
+import 'package:to_do_list_challenge/layers/presentation/pages/land/land_page_first.dart';
+import 'package:to_do_list_challenge/layers/presentation/pages/land/land_page_logged.dart';
 import 'package:to_do_list_challenge/layers/presentation/pages/tasks/home_task_page.dart';
 import 'package:to_do_list_challenge/layers/presentation/themes/app_theme.dart';
 import 'package:to_do_list_challenge/locator.dart';
@@ -41,7 +44,13 @@ class MyApp extends StatelessWidget {
               theme: themeToDoLight,
               darkTheme: themeToDoDark,
               themeMode: _themeManagerController.themeMode,
-              home: const HomeTaskPage(),
+              home: locator.get<LandPageController>().getUser() != null
+                  ? LandPageLogged(
+                      controller: locator(),
+                    )
+                  : LandPage(
+                      controller: locator(),
+                    ),
             );
           },
         );
